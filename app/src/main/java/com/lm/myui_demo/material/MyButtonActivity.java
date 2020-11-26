7259545d4f42400141ab5bd36343531ec9401ae2
TFly <lm@LMdeMacBook-Pro-2.local>
2020-11-26 17:45:58 +0800

    material button demo

---
+++ app/src/main/java/com/lm/myui_demo/material/MyButtonActivity.java
@@ -0,0 +1,14 @@
+package com.lm.myui_demo.material;
+
+import android.os.Bundle;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import com.lm.myui_demo.R;
+
+public class MyButtonActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_button);
+    }
+}
